1f7e3248a7a88b5acbbe94d78ccd2dfda2f7d113
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-05-22 23:54:24 +0530

    removed browser router and added hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Clock from './Components/Clock'
 import Stopwatch from './Components/Stopwatch'
 import Timer from './Components/Timer'
@@ -10,14 +10,14 @@ const App = () => {
       <center>
         <h1 className='heading'>Clock</h1>
       </center>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Clock/>}/>
           <Route path='/*' element={<center><h2>Page Not Found</h2></center>} />
           <Route path='/StopWatch' element={<Stopwatch/>} />
           <Route path='/Timer' element={<Timer/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
